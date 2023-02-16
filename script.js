a12f3a4bf093701d0145f45901c1f64c0e712390
pranaydarshi <101990641+pranaydarshi@users.noreply.github.com>
2023-02-16 20:49:05 +0530

    updated script.js

--- script.js
+++ script.js
@@ -11,6 +11,65 @@ function readDetails() {
 }
 let details = document.getElementById("details");
 let add = document.getElementById("add")
+details.innerHTML = `
+<style>
+         #values{
+  
+             margin-top: 140px;
+             margin-left: 100px;
+             width:450px;
+             height:210px;
+             text-align: center;
+
+            
+          } 
+          #values,.th,.td{
+            border:2px solid ;
+          }
+         
+          </style>
+        
+        <table id="values" > 
+            <thead>
+            <th colspan="4"><marquee>STUDENTS DATA</marquee></th>
+
+                    
+            </thead>
+            <tbody>
+            <th class="th" scope="col">NAME</th>
+                    <th class="th" scope="col">ROLLNO</th>
+                    <th class="th" scope="col">BRANCH</th>
+                    <th class="th" scope="col">COURSE</th>
+            </tbody>
+           
+        </table>
+
+`
+get(child(ref(db), "students")).then((snapshot) => {
+    let students = []
+    snapshot.forEach(childsnapshot => {
+        students.push(childsnapshot.val());
+
+    })
+    for (let i = 0; i < students.length; i++) {
+
+        let tr = document.createElement('tr');
+        let td1 = tr.appendChild(document.createElement('td'));
+        let td2 = tr.appendChild(document.createElement('td'));
+        let td3 = tr.appendChild(document.createElement('td'));
+        let td4 = tr.appendChild(document.createElement('td'));
+        td1.innerHTML = students[i].name;
+        td1.setAttribute("class", "td");
+        td2.innerHTML = students[i].rollno;
+        td2.setAttribute("class", "td");
+        td3.innerHTML = students[i].year;
+        td3.setAttribute("class", "td");
+        td4.innerHTML = students[i].branch;
+        td4.setAttribute("class", "td");
+        document.getElementById("values").appendChild(tr);
+    }
+
+}) 
 add.addEventListener("click", () => {
     readDetails();
 
@@ -96,62 +155,4 @@ del.addEventListener("click", () => {
         rollno
     })
 })
-details.innerHTML = `
-<style>
-         #values{
-  
-             margin-top: 140px;
-             margin-left: 100px;
-             width:450px;
-             height:210px;
-             text-align: center;
-
-            
-          } 
-          #values,.th,.td{
-            border:2px solid ;
-          }
-         
-          </style>
-        
-        <table id="values" > 
-            <thead>
-            <th colspan="4"><marquee>STUDENTS DATA</marquee></th>
-
-                    
-            </thead>
-            <tbody>
-            <th class="th" scope="col">NAME</th>
-                    <th class="th" scope="col">ROLLNO</th>
-                    <th class="th" scope="col">BRANCH</th>
-                    <th class="th" scope="col">COURSE</th>
-            </tbody>
-           
-        </table>
-
-`
-get(child(ref(db), "students")).then((snapshot) => {
-    let students = []
-    snapshot.forEach(childsnapshot => {
-        students.push(childsnapshot.val());
-
-    })
-    for (let i = 0; i < students.length; i++) {
-
-        let tr = document.createElement('tr');
-        let td1 = tr.appendChild(document.createElement('td'));
-        let td2 = tr.appendChild(document.createElement('td'));
-        let td3 = tr.appendChild(document.createElement('td'));
-        let td4 = tr.appendChild(document.createElement('td'));
-        td1.innerHTML = students[i].name;
-        td1.setAttribute("class", "td");
-        td2.innerHTML = students[i].rollno;
-        td2.setAttribute("class", "td");
-        td3.innerHTML = students[i].year;
-        td3.setAttribute("class", "td");
-        td4.innerHTML = students[i].branch;
-        td4.setAttribute("class", "td");
-        document.getElementById("values").appendChild(tr);
-    }
-
-})  
+ 
